08bda8fadebe2a4276adb127fb7145f3d5fb449c
Beatriz <beatriz.abad@estudiant.fjaverianas.com>
2023-11-10 11:51:04 +0100

    programa_sumes

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ internal class Program
  private static void Main(string[] args)
  {
  int number1, number2, total;
- Console.WriteLine("Dona'm el primer valor: ");
+ Console.WriteLine("Dona'm número: ");
  number1 = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Dona'm el segon valor: ");
+ Console.WriteLine("Dona'm un altre número: ");
  number2 = Convert.ToInt32(Console.ReadLine());
  total = number1 + number2;
- Console.WriteLine("la suma correcta és: " + total);
+ Console.WriteLine("la suma és " + total);
  Console.ReadLine();
  }
 }
